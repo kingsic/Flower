ace9b7f729dddaed80dd499dc5591f404570f82c
哥哥帅 <1357127436@qq.com>
2019-06-19 19:28:36 +0800

    【代码介绍】

--- README.md
+++ README.md
@@ -4,6 +4,25 @@
 * 本项目中的 SGTagsView 来源于对 [SGPagingView](https://github.com/kingsic/SGPagingView) 框架中的 SGPageTitleView 的衍生
 
 
+## Installation
+* 1、CocoaPods 导入 pod 'SGRichView', '~> 0.0.1'
+* 2、下载、拖拽 “SGRichView” 文件夹到工程中
+
+## 代码介绍
+#### SGPagingView 的使用（详细使用, 请参考 Demo）
+``` 
+    SGTagsViewConfigure *configure = [SGTagsViewConfigure tagsViewConfigure];
+
+    NSArray *tags = @[@"iPhone XS", @"iPhone XS Max", @"iPhone X", @"iPhone XR", @"iPhone 8", @"iPhone 8P"];
+    SGTagsView *tagsView = [SGTagsView tagsViewWithFrame:CGRectMake(0, 200, self.view.frame.size.width, 50) configure:configure];
+    tagsView.tags = tags;
+    [self.view addSubview:tagsView];
+    tagsView.selectedBlock = ^(SGTagsView * _Nonnull tagsView, NSString *tag, NSInteger index) {
+        NSLog(@"%@ - - %ld", tag, index);
+    };
+```
+
+
 ### SGRichView 实现的界面效果图<br>
 ![](https://github.com/kingsic/Useless/blob/master/SGRichView/SGRichView.png)
 
--- SGRichViewExample/ViewController.m
+++ SGRichViewExample/ViewController.m
@@ -20,22 +20,25 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     SGTagsViewConfigure *configure = [SGTagsViewConfigure tagsViewConfigure];
-    configure.tagsViewStyle = SGTagsViewStyleEquable;
+    configure.tagsViewStyle = SGTagsViewStyleVertical;
     configure.cornerRadius = 15;
     configure.borderWidth = 2;
-//    configure.column = 4;
+    configure.selectedColor = [UIColor whiteColor];
+    configure.borderColor = [UIColor redColor];
+    configure.selectedBorderColor = [UIColor greenColor];
+    configure.selectedBackgroundColor = [UIColor redColor];
 //    configure.multipleSelected = YES;
     
+    NSArray *tags = @[@"iPhone XS", @"iPhone XS Max", @"iPhone X", @"iPhone XR", @"iPhone 8", @"iPhone 8P"];
     SGTagsView *tagsView = [SGTagsView tagsViewWithFrame:CGRectMake(0, 200, self.view.frame.size.width, 50) configure:configure];
-    tagsView.backgroundColor = [UIColor greenColor];
-    tagsView.tags = @[@"iPhone XS", @"iPhone XS Max", @"iPhone X", @"iPhone XR", @"iPhone 8", @"iPhone 8P"];
+    tagsView.tags = tags;
     [self.view addSubview:tagsView];
     tagsView.selectedBlock = ^(SGTagsView * _Nonnull tagsView, NSString *tag, NSInteger index) {
         NSLog(@"%@ - - %ld", tag, index);
     };
-    tagsView.multipleSelectedBlock = ^(SGTagsView * _Nonnull tagsView, NSArray * _Nonnull tags, NSArray * _Nonnull indexs) {
-        NSLog(@"%@ - - %@", tags, indexs);
-    };
+//    tagsView.multipleSelectedBlock = ^(SGTagsView * _Nonnull tagsView, NSArray * _Nonnull tags, NSArray * _Nonnull indexs) {
+//        NSLog(@"%@ - - %@", tags, indexs);
+//    };
 }
 
 
